3cee11967cdf78804b74603ac28957aec0ab08bd
Felipe Uribe <felipenorg@hotmail.es>
2024-08-26 00:48:37 -0500

    Exception was created to hanlde custom errors and constants were created to avoid magical strings

---
+++ utils/constants.py
@@ -0,0 +1,11 @@
+DB_NAME = "habi_db"
+PRE_SALE_STATUS = "pre_venta"
+SALE_STATUS = "en_venta"
+SOLD_STATUS = "vendido"
+REST_GENERAL_HEADERS = {
+    "content-type": "application/json",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*"
+}
+TOKEN_PREFIX = "Token "
---
+++ utils/exceptions.py
@@ -0,0 +1,10 @@
+class CustomBadRequest(Exception):
+    pass
+
+
+class CustomUnauthorized(Exception):
+    pass
+
+
+class CustomMethodNotAllowed(Exception):
+    pass
